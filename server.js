142f53b4e0efb93849edef6c1b86f0716b536c82
Ishalini <87859854+IT20134280@users.noreply.github.com>
2022-07-22 15:58:50 +0530

    mongoDB connected

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const fileUpload = require('express-fileupload')
+const path = require('path')
 
 const app = express()
 app.use(express.json())
@@ -13,9 +14,15 @@ app.use(fileUpload({
     useTempFile: true
 }))
 
-app.use('/', (req, res, next) => {
-    res.json({msg: "Hello Everyone!"})
+//Connect to the mongodb
+const URI = process.env.MONGODB_URL
+mongoose.connect(URI, {
+    useNewUrlParser: true,
 })
+.then(() => {
+    console.log("Connected to mongodb");
+})
+.catch((e) => console.log(e));
 
 const PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
