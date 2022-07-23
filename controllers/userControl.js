a9b6dee0fd2f523c0fd81de1735ef4de77ce4967
Ishalini <87859854+IT20134280@users.noreply.github.com>
2022-07-23 10:45:44 +0530

    initial update

--- controllers/userControl.js
+++ controllers/userControl.js
@@ -1,15 +1,60 @@
 const Users = require('../models/userModel')
+const bcrybt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const SendmailTransport = require('nodemailer/lib/sendmail-transport')
+
+const { CLIENT_URL } = process.env
 
 const userControl = {
-    register: async(req, res) => {
+    register: async (req, res) => {
         try {
-            console.log(req.body)
-            res.json ({msg: "Register Test"})
+            const { name, email, password } = req.body
+            //Return Error Messages 
+            if (!name || !email || !password)
+                return res.status(400).json({ msg: "Please fill in all fields" })
+            if (!validateEmail(email))
+                return res.status(400).json({ msg: "Invail email" })
+
+            const user = await Users.findOne({ email })
+            if (user) return res.status(400).json({ msg: "This email already exists" })
+
+            if (password.length > 6)
+                return res.status(400).json({ msg: "Passwors must be at least 6 characters" })
+
+            //Password field -- invisible password
+            const passwordHash = await bcrybt.hash(password, 12)
+
+            const newUser = {
+                name, email, password: passwordHash
+            }
+
+            const activation_token = createActivationToken(newUser)
+
+            const url = '${CLIENT_URL}/user/activate/${activation_token}'
+            SendmailTransport(email, url)
+
+
+
+            res.json({ msg: "Register Success! Please activate your email to start." })
         } catch (err) {
-            return res.status(500).json({msg: err.message})
+            return res.status(500).json({ msg: err.message })
         }
     }
 }
 
+function validateEmail(email) {
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
+}
+//Generate Secret password
+const createActivationToken = (payload) => {
+    return jwt.sign(payload, process.env.ACTIVATION_TOKEN_SECRET, { expiresIn: '5m' })
+}
+const createAccessToken = (payload) => {
+    return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '14m' })
+}
+const createRefreshToken = (payload) => {
+    return jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '7d' })
+}
 
 module.exports = userControl
